39eff28b99399c359d8131da708e60c9016ff46d
spirit-tv <tlitaudon@gmail.com>
2020-06-02 14:29:22 +0200

    mise a jour des noms de fichiers

---
+++ HomePage/index.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Projet Fils Rouge</title>
+
+    <!-- Favico -->
+    <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/eoliennes.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/shema.css">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Projet fils rouge</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">À propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#projects">Projet Low Tech</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#actions">Conception</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <h1 class="mx-auto my-0 text-uppercase">Dans le vent</h1>
+          <h2 class="text-grey mx-auto mt-5 mb-5">Projet fils rouge 2019-2020 Edenschool France</h2>
+          <div>
+            <img src="img/eolienne.png" alt="eolienne" class="full"/>
+          </div>
+	  <div id="scroll">
+	    <a href="#about" class="js-scroll-trigger"><span></span></a>
+	  </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- About Section -->
+ <section id="about" class="about-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2 class="text-black-50 mb-5">Projet fils rouge</h2>
+            <p class="text-black-50 mt-5 mb-0 mx-3">Le projet fils rouge est un projet qui nous a etait demande en debut de deuxieme année chez edenschool 
+          </div>
+        <div class="col-lg-8 mx-auto">
+            <h2 class="text-black-50 mb-4"></h2>
+            <p class="text-black-50 mb-0  mx-3">
+Notre mission est simple nous devions crée une éolienne pour pouvoir charger notre téléphone avec cela nous devions délivrer un site qui présente ce projet avec un blog.
+          </div>
+        </div>
+      </div>
+</section>
+
+
+    <!-- Projects Section -->
+    <section id="projects" class="projects-section bg-light">
+      <div class="container">
+
+	<!-- Title -->
+        <div class="row">
+          <div class="col-lg-8 mt-0 mx-auto mb-5">
+            <h2 class="text-black-50 text-center mb-3">Les dérives</h2>
+          </div>
+	</div>
+
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-7 col-lg-6">
+            <img class="img-fluid mb-3 mb-lg-0" src="img/cows.jpg" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Une approche écologique</h4>
+              <p class="text-black-50 mb-0">Nous utilisont tous nos téléphone au moin plus de une heure dans la journée donc cela consomme de la battrie et pour le recharger écologiquement rien de mieux qu'une éolienne</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+ <!-- Actions Section -->
+    <section id="actions" class="projects-section bg-white">
+      <div class="container">
+
+	<!-- Title -->
+        <div class="row">
+          <div class="col-lg-8 mt-0 mx-auto mb-5">
+            <h2 class="text-black-50 text-center mb-3">Conception</h2>
+          </div>
+	</div>
+
+	<!-- First One Row -->		
+  <section class="o-section">
+    <div class="c-hotspot-image">
+      <div class="c-hotspot-image__visual">
+        <img class="shema"
+          src="https://cdn.discordapp.com/attachments/692286899343065099/710404683012898836/1200px-Schema_eolienne.svg.png"
+          alt="">
+      </div>
+      <div class="c-hotspot-image__hotspots js-hotspot-container">
+        <!-- HOTSPOT -->
+        <div class="c-hotspot c-hotspot--1 c-hotspot--top js-hotspot" style="left: 25%; top: 26%">
+          <span class="c-hotspot__circle js-link js-hotspot-circle">
+            <i class="c-hotspot__circle-inner">
+  
+            </i>
+          </span>
+  
+          <div class="c-hotspot__text js-hotspot-text">
+            <article>
+              <h4 class="u-a1 js-header-title">
+                  
+              </h4>
+              <p class="u-b2">
+              <a id="gris">Gris:</a>Moyeu et commande du rotor <br>
+              <a id="rouge">Rouge:</a>  Pale   
+             </p>
+            </article>
+          </div>
+        </div>
+        <!-- //HOTSPOT -->
+  
+        <!-- HOTSPOT -->
+        <div class="c-hotspot c-hotspot--2 c-hotspot--top js-hotspot" style="left: 51%; top: 26%">
+          <span class="c-hotspot__circle js-link js-hotspot-circle">
+            <i class="c-hotspot__circle-inner">
+  
+            </i>
+          </span>
+  
+          <div class="c-hotspot__text js-hotspot-text">
+            <article>
+              <h4 class="u-a1 js-header-title">
+                
+              </h4>
+              <p class="u-b2">
+               <a id="gris">Gris:</a>  Nacelle <br>
+               <a id="bleu">Bleu:</a>   Générateur <br>
+               <a id="">Blanc:</a>   Systeme de regulation éléctrique
+              </p>
+            </article>
+          </div>
+        </div>
+        <!-- //HOTSPOT -->
+  
+        <!-- HOTSPOT -->
+        <div class="c-hotspot c-hotspot--3 c-hotspot--bottom js-hotspot" style="left: 40%; top: 68%">
+          <span class="c-hotspot__circle js-link js-hotspot-circle" style="transform: translate(0px, 0px);">
+            <i class="c-hotspot__circle-inner">
+  
+            </i>
+          </span>
+  
+          <div class="c-hotspot__text js-hotspot-text" style="transform: translate(0px, 0px);">
+            <article>
+              <h4 class="u-a1 js-header-title">
+                </h4>
+              <p class="u-b2">
+                <a id="peche"> Peche:</a> Fondation <br>
+                <a id="jaune">Jaune:</a> Armoir de couplage au réseau éléctrique <br>
+                <a id="vert-claire">Vert:</a> Mat
+              </p>
+            </article>
+          </div>
+        </div>
+        <!-- //HOTSPOT -->
+  
+        <!-- HOTSPOT -->
+        <div class="c-hotspot c-hotspot--4 c-hotspot--bottom js-hotspot" style="left: 40%; top: 28%">
+          <span class="c-hotspot__circle js-link js-hotspot-circle" style="transform: translate(0px, 0px);">
+            <i class="c-hotspot__circle-inner">
+  
+            </i>
+          </span>
+  
+          <div class="c-hotspot__text js-hotspot-text" style="transform: translate(0px, 0px);">
+            <article>
+              <h4 class="u-a1 js-header-title">
+                </h4>
+              <p class="u-b2">
+                <a id="vert">Vert:</a> Frein <br>
+                <a id="">Noire:</a> Multiplicateur <br>
+                <a id="vert-claire">Vert:</a> Systeme d'orientation
+
+              </p>
+            </article>
+          </div>
+        </div>
+        <!-- //HOTSPOT -->
+  </canvas>
+      </div>
+  
+    </div>
+  </section>
+      </div>
+    </section>
+
+    <!-- Projects Section -->
+    <section id="projects" class="projects-section bg-light">
+      <div class="container">
+
+	<!-- Title -->
+        <div class="row">
+          <div class="col-lg-8 mt-0 mx-auto mb-5">
+            <h2 class="text-black-50 text-center mb-3">Journal de bord</h2>
+          </div>
+          <div class="col-lg-10 offset-lg-1 pt-5 pb-5 text-black-50 shadow-lg p-3 mb-5 backgroundtu rounded">
+            <div id="client-testimonial-carousel" class="carousel slide" data-ride="carousel" style="height:200px;">
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active text-center p-4">
+                  <blockquote class="blockquote text-center">
+                    <p class="mb-0"><i class="fa fa-quote-left"></i> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, necessitatibus! Magnam quis soluta, quae autem, 
+                    </p>
+                    <footer class="blockquote-footer">Tom Litaudon <cite title="Source Title">developpeur</cite></footer>
+                    <!-- Client review stars -->
+                    <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                    
+                  </blockquote>
+                </div>
+                <div class="carousel-item text-center p-4">
+                  <blockquote class="blockquote text-center">
+                    <p class="mb-0"><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim illum accusantium, 
+                    </p>
+                    <footer class="blockquote-footer">Alann <cite title="Source Title">developpeur</cite></footer>
+                    <!-- Client review stars -->
+                    <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                    
+                  </blockquote>
+                </div>
+                <div class="carousel-item text-center p-4">
+                  <blockquote class="blockquote text-center">
+                    <p class="mb-0"><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus distinctio perferendis fuga necessitatibus similique corporis assumenda, 
+                    </p>
+                    <footer class="blockquote-footer">Hassen <cite title="Source Title">developpeur</cite></footer>
+                    <!-- Client review stars -->
+                    <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                   
+                  </blockquote>
+                </div>
+              </div>
+              <ol class="carousel-indicators">
+                <li data-target="#client-testimonial-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#client-testimonial-carousel" data-slide-to="1"></li>
+                <li data-target="#client-testimonial-carousel" data-slide-to="2"></li>
+              </ol>
+            </div>
+          </div>
+	</div>
+
+
+        <!-- Featured Project Row -->
+        
+
+      </div>
+    </section>
+
+    <!-- Signup Section -->
+    <section id="signup" class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+            <!--<i class="far fa-paper-plane fa-2x mb-2 text-white"></i>-->
+            <h2 class="text-white mb-5">Suivez-nous pour recevoir nos update!</h2>
+
+            <form class="form-inline d-flex">
+              <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Entrez votre email...">
+              <button type="submit" class="btn btn-primary mx-auto">Ok</button>
+            </form>
+
+          </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact-section bg-black">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Adresse</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">50 Cours de la République, 69100 Villeurbanne</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">
+                  <a href="#">contact@eoliennes.org</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Telephone</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">09 81 78 68 85</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Pour Edenschool France
+      </div>
+      <div class="container">
+        Par Tom litaudon, Hassen, Alann
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/eoliennes.min.js"></script>
+    <script src="js/shema.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.2.1/pixi.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.2/gsap.min.js'></script>
+
+  </body>
+
+</html>
