9ec92de10ea436d807bfb91b7d53198c4c7a9f8e
spirit-tv <tlitaudon@gmail.com>
2020-06-08 14:43:45 +0200

    ajout de la view edite article peut etre a changer

---
+++ MVC/src/Views/blog/edite-blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Edite article</title>
+  <link rel="stylesheet" href="css/edite.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>Edite Artcile</h3>
+    <h4>Ici tu peux édite ton article</h4>
+    <fieldset>
+      <input placeholder="Titre" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="ton article" tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Envoyer</button>
+    </fieldset>
+  </form>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
